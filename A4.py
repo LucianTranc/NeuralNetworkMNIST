2e63c6feb678a0cecb47e92e1ccd174559a53715
LucianTranc <ltranc@uoguelph.ca>
2023-04-12 21:02:40 -0400

    finished forward propagation?

--- A4.py
+++ A4.py
@@ -1,6 +1,10 @@
 import numpy as np
 import random
 
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
 #    nodes = forward_propogation(input, nodesPerLayer, weights, biases)
 
 def forward_propogation(input, nodesPerLayer, weights, biases):
@@ -10,20 +14,14 @@ def forward_propogation(input, nodesPerLayer, weights, biases):
     for nodeCount in nodesPerLayer:
         nodes.append(np.zeros(nodeCount, float))
     
+    # set first layer to input
     nodes[0] = input
 
-    nodeIndex = 0
-    layerIndex = 1
-    nodes[layerIndex][nodeIndex] = nodes[layerIndex-1] * weights[0]
-    
-
-
-
-
-    return 0
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+    # set the value of each node based on the preivous layer's values, the weights and the biases.
+    for layerIndex in range(1, len(nodesPerLayer)):
+        for nodeIndex in range(0, len(weights[layerIndex - 1])):
+            nodes[layerIndex][nodeIndex] = sigmoid(np.sum(np.multiply(nodes[layerIndex - 1], weights[layerIndex - 1][nodeIndex])) + biases[layerIndex-1][nodeIndex])
+    return nodes
     
 print("Lucian Tranc's MNIST Nerual Network")
 
@@ -43,7 +41,7 @@ nodesPerLayer = [28*28, 16, 16, 10]
 weights = []
 
 for i in range(1, len(nodesPerLayer)):
-    weights.append(sigmoid(np.random.randn(nodesPerLayer[i], nodesPerLayer[i-1])))
+    weights.append(np.random.rand(nodesPerLayer[i], nodesPerLayer[i-1]) - 0.5)
 
 print("Weights:")
 print(weights)
@@ -51,7 +49,7 @@ print(weights)
 biases = []
 
 for i in range(1, len(nodesPerLayer)):
-    biases.append(sigmoid(np.random.randn(nodesPerLayer[i])))
+    biases.append(np.random.randn(nodesPerLayer[i]))
 
 print("Biases:")
 print(biases)
@@ -66,25 +64,17 @@ print(biases)
 
 # need to do vector multiplication between weights[0][node] and d
 
-print("WEIGHTS[0][0]")
-print(weights[0][0])
-print(len(weights[0][0]))
+# for d in data:
+label = data[0][0]
+input = data[0][1]
 
-print("DATA[0][1]")
-print(data[0][1])
-print(len(data[0][1]))
+# At this point I have a list of nodes where the first layer is set to the input.
+# I also have a sets of random weights and biases.
+# I want to run a forward propogation. the forward propogation needs to keep the values
+# of the nodes.
 
-for d in data:
-    label = d[0]
-    input = d[1]
-
-    # At this point I have a list of nodes where the first layer is set to the input.
-    # I also have a sets of random weights and biases.
-    # I want to run a forward propogation. the forward propogation needs to keep the values
-    # of the nodes.
-
-    # gets the state of all the nodes
-    nodes = forward_propogation(input, nodesPerLayer, weights, biases)
+# gets the state of all the nodes
+nodes = forward_propogation(input, nodesPerLayer, weights, biases)
 
 
 
